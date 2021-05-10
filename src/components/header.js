5018431b2a7c8246b5212a11ff9dd684c74a81a3
Kunal <kdshah6593@gmail.com>
2021-05-10 19:17:50 -0400

    css formatting

--- src/components/header.js
+++ src/components/header.js
@@ -3,50 +3,31 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle, menuLinks }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header style={{ background: `#000`, marginBottom: `1rem`, display: `flex`, flexDirection: `row`, justifyContent: `space-between`}}>
+    <div style={{ marginLeft: `1rem`, paddingTop: `0.5rem` }}>
+      <h1 style={{ paddingTop: `1rem` }}>
+        <Link to="/" style={{ color: `#6184d4`, textDecoration: `none`, }}>
           {siteTitle}
         </Link>
       </h1>
     </div>
-    <div>
-        <nav>
-          <ul style={{ display: "flex", flex: 1 }}>
-            {menuLinks.map(link => (
-              <li
-                key={link.name}
-                style={{
-                  listStyleType: `none`,
-                  padding: `1rem`,
-                }}
-              >
-                <Link style={{ color: `white` }} to={link.link}>
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+    <nav>
+      <ul style={{ display: "flex", flexDirection: `row`, paddingTop: `1rem` }}>
+        {menuLinks.map(link => (
+          <li
+            key={link.name}
+            style={{
+              listStyleType: `none`,
+              padding: `1rem 1rem 0rem 1rem`,
+            }}
+          >
+            <Link style={{ color: `#6184d4` }} to={link.link}>
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   </header>
 )
 
