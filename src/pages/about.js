b156d3192998cdd6f41c27c2283615ea9518fdb8
Kunal <kdshah6593@gmail.com>
2021-05-11 18:15:33 -0400

    about me page photo text, flexbox css

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,32 @@
 import React from 'react';
-
+import "../components/styles.css"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 
 const About = () => {
     return (
         <Layout>
             <Seo title="About" />
-                <div>
-                    <h1>About Me</h1>
-                    <p>Well, nothing here yet.</p>
+                <div id="about-body">
+                    <div id="about-container">
+                        <StaticImage
+                            src="../images/kunal-shah-greyed.png"
+                            width={900}
+                            quality={95}
+                            formats={["AUTO", "WEBP", "AVIF"]}
+                            alt="Kunal Shah Profile Picture"
+                            className="about-container-element"
+                        />
+                        <div className="about-container-element" id="about-text-container">
+                            <h1 className="marker-font" id="about-header">About Me.</h1>
+                            <p style={{paddingRight: `5rem` }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                    </div>
+                    <div id="about-technical-skills">
+                        <h2 className="centered text-color marker-font" id="tech-skills-header">Technical Skills</h2>
+                    </div>
                 </div>
         </Layout>
     )
