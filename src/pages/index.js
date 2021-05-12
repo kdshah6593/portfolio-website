2197ad0dbd2a53599a523823b731437340eac29b
Kunal <kdshah6593@gmail.com>
2021-05-12 18:01:58 -0400

    add separating lines, css lines, font size for text, vertical center text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,13 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1 className="marker-font" id="index-header">Hi, I'm Kunal</h1>
-    <h3 className="text-color centered nova-font">Full Stack Web Developer</h3>
-    <p className="text-color centered nova-font">Let's build something great together.</p>
+    <div id="index-body">
+      <h1 className="marker-font" id="index-header">Hi, I'm Kunal</h1>
+      <hr style={{ width:`50%`, height: `5px`, marginLeft:`25%`, backgroundColor:`#D48C63` }}></hr>
+      <h3 id="index-subheader">Full Stack Web Developer</h3>
+      <hr style={{ width:`50%`, height: `2px`, marginLeft:`25%`, backgroundColor:`#D48C63` }}></hr>
+      <h5 id="index-subtitle">Let's build something great together.</h5>
+    </div>
   </Layout>
 )
 
