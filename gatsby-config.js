3b2e73c7124ffa61292a019d8d629b6a87aa0ff0
Kunal <kdshah6593@gmail.com>
2021-05-18 14:32:33 -0400

    styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
