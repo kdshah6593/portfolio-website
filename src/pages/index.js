89e993097a5c7df7ac477e5ef5a9808c621c7b27
Kunal <kdshah6593@gmail.com>
2021-05-19 19:33:59 -0400

    animation for headings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import "../components/styles.css"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -9,9 +8,9 @@ const IndexPage = () => (
     <Seo title="Home" />
     <div id="index-body">
       <h1 className="marker-font" id="index-header">Hi, I'm Kunal</h1>
-      <hr style={{ width:`50%`, height: `5px`, marginLeft:`25%`, backgroundColor:`#D48C63` }}></hr>
+      <hr className="drop-down1" style={{ width:`50%`, height: `5px`, marginLeft:`25%`, backgroundColor:`#D48C63` }}></hr>
       <h3 id="index-subheader">Full Stack Web Developer</h3>
-      <hr style={{ width:`50%`, height: `2px`, marginLeft:`25%`, backgroundColor:`#D48C63` }}></hr>
+      <hr className="drop-down2" style={{ width:`50%`, height: `2px`, marginLeft:`25%`, backgroundColor:`#D48C63` }}></hr>
       <h5 id="index-subtitle">Let's build something great together.</h5>
     </div>
   </Layout>
