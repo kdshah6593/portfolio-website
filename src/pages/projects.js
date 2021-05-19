d3e3325664f54948d93e789efc06970dafd3231f
Kunal <kdshah6593@gmail.com>
2021-05-19 16:04:54 -0400

    project photos, descriptions, demo links

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,6 +5,10 @@ import Seo from "../components/seo"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import StudyDecks from "../images/studydecks_main.png"
+import SportsReport from "../images/sports-report-articles.png"
+import StockTracker from "../images/stock_tracker.png"
+import FiTracker from "../images/fitracker.png"
+import LOLChampions from "../images/lolchamps.png"
 
 const CardContainer = styled.div`
 display: flex;
@@ -35,7 +39,7 @@ width: 60%;
 
 const CardImage = styled.img`
 width: 40%;
-height: 90%;
+height: 250px;
 padding: 5px;
 border-radius: 10px;
 margin: 0;
@@ -98,6 +102,7 @@ const NotLiveProject = (props) => (
             <CardDescription>{props.description}</CardDescription>
             <LinkWrapper>
                 <CardLink to={props.github} target="_blank">Source Code</CardLink>
+                <CardLink to={props.demo} target="_blank">Demo</CardLink>
             </LinkWrapper>
         </CardText>
     </CardWrapper>
@@ -112,15 +117,15 @@ const Projects = () => {
                 <CardContainer>
                     <LiveProject
                         title="The Sports Report"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        description="The Sports Report is a web application developed using React.js and Rails that provides curated sports articles towards your favorite players and teams. Instead of searching various news sources, all the sources are combined into one place."
                         frontend="https://github.com/kdshah6593/the-sports-report-frontend"
                         backend="https://github.com/kdshah6593/the-sports-report-backend"
                         hosted="https://the-sports-report.herokuapp.com"
-                        image={StudyDecks}
+                        image={SportsReport}
                     />
                     <LiveProject
                         title="StudyDecks"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        description="StudyDecks is a web application developed using JavaScript on the Frontend and Rails as an API as the backend. This web app is intended as a simple study tool for users. A user can have multiple decks and within each deck they can create flashcards and go between them. A user can flip the flashcard over, thus they can quiz themselves."
                         frontend="https://github.com/kdshah6593/studydecks-frontend"
                         backend="https://github.com/kdshah6593/studydecks-backend"
                         hosted="https://studydecks.herokuapp.com"
@@ -128,21 +133,24 @@ const Projects = () => {
                     />
                     <NotLiveProject
                         title="Stock Tracker"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                        image={StudyDecks}
+                        description="Stock Tracker is a web application developed using Ruby on Rails incorporating CRUD and MVC principles. This web app is intended for users to keep track of their many stock portfolios in one place maintain watchlists, and view stock information."
+                        image={StockTracker}
                         github="https://github.com/kdshah6593/Stock-Tracker"
+                        demo="https://drive.google.com/file/d/1uMDyndoxl7-d373by1DjaFMtbO7DHJJ4/view?usp=sharing"
                     />
                     <NotLiveProject
                         title="FiTracker"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                        image={StudyDecks}
+                        description="FITracker is a web application developed using Sinatra and ActiveRecord incorporating CRUD and MVC principles. The application can sign up and login users who can add workouts to their profile. Per individual workout, a user can add exercises. Users can view others' exercises as well."
+                        image={FiTracker}
                         github="https://github.com/kdshah6593/FITracker"
+                        demo="https://drive.google.com/file/d/1JbTliZkDOcBSHOe_QzTgkxQUMW5b3OzN/view?usp=sharing"
                     />
                     <NotLiveProject
-                        title="LOL Champions"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                        image={StudyDecks}
+                        title="League of Legends Champion Finder"
+                        description="This is a Command Line Interace program. New to League of Legends or just want some quick information on the Champions in League of Legends? Find out more information on the League of Legends champions to know who you want to play."
+                        image={LOLChampions}
                         github="https://github.com/kdshah6593/League_of_Legends_Champions"
+                        demo="https://drive.google.com/file/d/12dwHdkLuvR-bE2Yt--qefBkHF-kg5Me_/view?usp=sharing"
                     />
                 </CardContainer>
             </div>
