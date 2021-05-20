1ee1e13c1d8fccb732e1df472ad832ae106fab3a
Kunal <kdshah6593@gmail.com>
2021-05-20 18:41:23 -0400

    about page text transitions

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,17 +21,19 @@ const About = () => {
             <Seo title="About" />
             <div id="about-body">
                 <div id="about-container">
-                    <StaticImage
-                        src="../images/kunal-shah-greyed.png"
-                        width={900}
-                        quality={95}
-                        formats={["AUTO", "WEBP", "AVIF"]}
-                        alt="Kunal Shah Profile Picture"
-                        className="about-container-element"
-                    />
+                    <div id="about-profile-image">
+                        <StaticImage
+                            src="../images/kunal-shah-greyed.png"
+                            width={900}
+                            quality={95}
+                            formats={["AUTO", "WEBP", "AVIF"]}
+                            alt="Kunal Shah Profile Picture"
+                            className="about-container-element"
+                        />
+                    </div>
                     <div className="about-container-element" id="about-text-container">
                         <h1 className="marker-font" id="about-header">About Me.</h1>
-                        <p style={{paddingRight: `5rem` }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <p id="about-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
                     </div>
                 </div>
