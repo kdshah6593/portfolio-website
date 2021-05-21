0c81a0accd912e43a4baeaff975a73861c6ed43c
Kunal <kdshah6593@gmail.com>
2021-05-21 15:38:15 -0400

    changed site metadata description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Kunal's Portfolio Site`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Kunal - Full Stack Web Developer - Portfolio Website built using GatsbyJS`,
+    author: `Kunal Shah`,
     menuLinks:[
       {
         name: 'About',
